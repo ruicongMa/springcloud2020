2060228a5215acc72e092b78bf7d2a8a38238515
Mark <mrc666888@gmail.com>
2020-03-26 18:14:18 +0800

    搭建Eureka单机群

--- cloud-consumer-order80/src/main/java/com/mrc/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/mrc/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.mrc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Mark
  * @date 2020/3/26 11:48
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- cloud-provider-payment8001/src/main/java/com/mrc/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/mrc/springcloud/PaymentMain8001.java
@@ -3,9 +3,11 @@ package com.mrc.springcloud;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.mrc.springcloud.dao")
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
