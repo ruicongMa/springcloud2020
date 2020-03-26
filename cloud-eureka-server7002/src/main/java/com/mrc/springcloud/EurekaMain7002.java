214990a400d059d5990e038901056e9b63c6b440
Mark <mrc666888@gmail.com>
2020-03-26 18:37:30 +0800

    Eureka集群搭建

---
+++ cloud-eureka-server7002/src/main/java/com/mrc/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.mrc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Mark
+ * @date 2020/3/26 15:54
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
