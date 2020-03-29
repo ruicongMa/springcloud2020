e6e7d1bf9dae55607e8d1f623bcc5dfaa011daa0
Mark <mrc666888@gmail.com>
2020-03-29 22:15:08 +0800

    搭建HystrixDashboard图形化监控平台，号称豪猪哥。

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/mrc/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.mrc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Mark
+ * @date 2020/3/29 21:46
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+
+}
