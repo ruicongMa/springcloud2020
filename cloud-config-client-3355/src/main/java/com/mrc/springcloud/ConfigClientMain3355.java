81abba8719f76ede8a179402a7e31e87b01d760e
Mark <mrc666888@gmail.com>
2020-03-30 16:30:08 +0800

    config客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/mrc/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.mrc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Mark
+ * @date 2020/3/30 16:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
