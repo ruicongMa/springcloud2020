6157774fb7a20586818d38ced33f405d5d2e100f
Mark <mrc666888@gmail.com>
2020-03-30 21:28:45 +0800

    spring cloud stream 消息驱动之消费者实现

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/mrc/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,27 @@
+package com.mrc.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Mark
+ * @date 2020/3/30 21:10
+ */
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void message(Message<String> message) {
+        log.info("消费者{}，接收到的消息：{}", serverPort, message.getPayload());
+    }
+}
