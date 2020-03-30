fd7a9b74a43eb6dcfb5e611bfb660acf53d255a0
Mark <mrc666888@gmail.com>
2020-03-30 17:15:56 +0800

    config动态刷新手动版

--- cloud-config-client-3355/src/main/java/com/mrc/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/mrc/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.mrc.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/3/30 16:21
  */
 @RestController
+@RefreshScope //动态刷新
 public class ConfigClientController {
 
     @Value("${config.info}")
