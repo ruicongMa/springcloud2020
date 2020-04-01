dc5c378b9edd4b41f97d83d7637b4dd74fb809e5
Mark <mrc666888@gmail.com>
2020-04-01 15:04:11 +0800

    Sentinel规则的测试

--- cloud-alibaba-sentinel-service8401/src/main/java/com/mrc/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/mrc/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.mrc.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,8 +20,18 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-
         return "------testB";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHandler")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "************ testHotKey ok!";
+    }
+
+    public String deal_testHandler(String p1, String p2, BlockException exception) {
+        return "************ deal_testHandler (灬ꈍ ꈍ灬)"; //sentinel系统默认的提示：Blocked by Sentinel (flow limiting)
     }
 
 }
